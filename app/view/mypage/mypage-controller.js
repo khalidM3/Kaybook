90d1c8e622335f5efcd7029b63e74645231078f6
Khalid Mohamud <kmohamud1@outlook.com>
2017-04-28 14:45:27 -0700

    mypage added

---
+++ app/view/mypage/mypage-controller.js
@@ -0,0 +1,29 @@
+'use strict';
+
+require('./_mypage.scss');
+
+module.exports = ['$log', '$rootScope', '$stateParams', 'profileService', MypageController];
+
+function MypageController($log, $rootScope, $stateParams, profileService) {
+  $log.debug('MypageController');
+
+  this.myUserID = $stateParams.userID;
+  this.loggedIn = true;
+
+  this.fetchProfile = function() {
+    $log.debug('MypageController.fetchProfile()');
+
+    profileService.fetchProfile(this.myUserID)
+    // .then(profile => recipeService.fetchMyRecipes(profile._id))
+    .then(prof => this.myProfile = prof);
+  };
+
+  // this.updateRecipeView = function() {
+  //   $log.debug('MyrecipesController.updateRecipeView()');
+
+  //   recipeService.fetchMyRecipes(this.myProfile._id)
+  //   .then(profile => this.myProfile = profile);
+  // };
+
+  this.fetchProfile();
+}
