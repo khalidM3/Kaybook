8069ea09f04e05d90712d95222481647dee71f10
Khalid Mohamud <kmohamud1@outlook.com>
2017-04-28 14:47:41 -0700

    signup view added

---
+++ app/view/signup/signup-controller.js
@@ -0,0 +1,29 @@
+'use strict';
+
+require('./_signup.scss');
+
+module.exports = ['$log', '$window', '$location', 'authService', 'profileService', SignupController];
+
+function SignupController($log, $window, $location, authService, profileService) {
+  $log.debug('SignupController');
+
+  this.say = function(){
+    console.log('WRONG INPUTS!');
+  };
+
+  this.signup = function(user) {
+    $log.debug('signupCtrl.signup');
+
+    authService.signup(user)
+    .then(res => {
+      let profile = {};
+      profile.name = res.config.data.username;
+      console.log('PROFILE', profile);
+      profileService.createProfile(profile)
+      .then(profile => {
+        $window.localStorage.setItem('userID', profile.userID);
+        $location.url(`/home/${profile.userID}` );
+      });
+    });
+  };
+}
