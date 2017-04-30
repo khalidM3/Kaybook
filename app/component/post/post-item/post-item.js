93ec0d7e50d03ccbe4d03bd22c8c0db8444508d1
Khalid Mohamud <kmohamud1@outlook.com>
2017-04-30 12:31:58 -0700

    added profile view

---
+++ app/component/post/post-item/post-item.js
@@ -0,0 +1,50 @@
+// 'use strict';
+
+// require('./_post-item.scss');
+
+// module.exports = {
+//   template: require('./post-item.html'),
+//   controller: ['$log', 'postService', 'profileService', PostItemController],
+//   controllerAs: 'postItemCtrl',
+//   bindings: {
+//     post: '<',
+//     loggedIn: '<',
+//     profile: '<',
+//     onpostChange: '&',
+//   }
+// };
+
+// function PostItemController($log, postService, profileService){
+//   $log.debug('postItemController');
+
+//   this.showEditpost = false;
+
+//   this.deletepost = function(post){
+//     $log.debug('postItemController.deletepost');
+
+//     postService.deletepost(post)
+//     .then(this.onpostChange());
+//   };
+
+//   this.poster = function() {
+//     $log.debug('postItemController.poster', this.post.posterPID);
+
+//     profileService.fetchProfile2(this.post.posterPID)
+//     .then(profile => this.poster = profile);
+//   };
+
+//   this.poster();
+
+//   this.$onInit = function() {
+//     $log.debug('postItemController.$onInit()');
+    
+//     if (this.post) return this.poster();
+//     return this.onpostChange();
+//   };
+
+//   this.updatepostItemView = function() {
+//     $log.debug('postItemController.updatepostItemView', this.post);
+
+//     this.onpostChange();
+//   };
+// }
