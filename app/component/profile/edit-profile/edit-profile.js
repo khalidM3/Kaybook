34c405e4433a2993c7f88f91afbc8f41f024f122
Khalid Mohamud <kmohamud1@outlook.com>
2017-05-11 23:31:51 -0700

    changed the edit profile component

--- app/component/profile/edit-profile/edit-profile.js
+++ app/component/profile/edit-profile/edit-profile.js
@@ -15,27 +15,51 @@ module.exports = {
 function EditProfileController($log, $window, profileService, picService) {
   $log.debug('EditProfileController');
 
+  // this.pic = {};
+  // let userID = $window.localStorage.getItem('userID');
+  // this.editProfile = function() {
+  //   profileService.editProfile(userID, this.profile)
+  //   .then( () => this.onProfileUpdated());
+  // };
+
   this.pic = {};
+  this.bannerPic = {};
+  let location = $window.location;
+
   let userID = $window.localStorage.getItem('userID');
   this.editProfile = function() {
+    
     profileService.editProfile(userID, this.profile)
-    .then( () => this.onProfileUpdated());
-  };
-
-  this.uploadProfilePic = function() {
-    let location = $window.location;
-
-    $log.debug('THIS PIC', this.pic);
-    picService.uploadProfilePic(this.profile, this.pic)
-    .then( () => {
+    .then( profile => {
+      if( !this.pic) return profile;
+      return picService.uploadProfilePic(profile, this.pic);
+    })
+    .then( profile => {
+      if( !this.bannerPic) return profile;
+      return picService.uploadProfilePic(profile, this.bannerPic);
+    })
+    .catch( () => {
       this.pic = null;
       location.reload(true);
       this.onProfileUpdated();
-    });
+    })
+    .then( () => this.onProfileUpdated());
   };
 
-  this.onProfileUpdated = function() {
+  // this.uploadProfilePic = function() {
+  //   let location = $window.location;
+
+  //   $log.debug('THIS PIC', this.pic);
+  //   picService.uploadProfilePic(this.profile, this.pic)
+  //   .then( () => {
+  //     this.pic = null;
+  //     location.reload(true);
+  //     this.onProfileUpdated();
+  //   });
+  // };
+
+  // this.onProfileUpdated = function() {
     
-  };
+  // };
 }
 
