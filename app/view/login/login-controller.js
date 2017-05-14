843b3f66ec572c7e1a66cfe630d53f89469b9d84
Khalid Mohamud <kmohamud1@outlook.com>
2017-05-13 22:11:27 -0700

    added fetch profile method the the login ctrl

--- app/view/login/login-controller.js
+++ app/view/login/login-controller.js
@@ -2,16 +2,17 @@
 
 require('./_login.scss');
 
-module.exports = ['$log', '$location', 'authService', 'profileService', LoginController];
+module.exports = ['$log', '$location', 'authService', LoginController];
 
-function LoginController($log, $location, authService, profileService) {
+function LoginController($log, $location, authService) {
   $log.debug('LoginController');
 
   this.myProfile = {};
+  this.user = {};
 
   this.login = function() {
     $log.debug('loginCtrl.login');
-
+    console.log('USER::', this.user);
     authService.login(this.user)
     .then(user => $location.url(`/home/${user._id}`))
     .catch( () => {
