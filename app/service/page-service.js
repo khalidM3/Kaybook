13a4bdc92a5d3c557636a11997d14b5f16cabf09
Khalid Mohamud <kmohamud1@outlook.com>
2017-05-24 14:37:18 -0700

    made page service

--- app/service/page-service.js
+++ app/service/page-service.js
@@ -35,7 +35,7 @@ function pageService($q, $log, $http, $window, authService) {
   };
 
   service.fetchPage = function(pageID) {
-    $log.debug('pageService.fetchProfile');
+    $log.debug('pageService.fetchPage');
 
     let url = `${__API_URL__}/api/page/${pageID}`;
     let config = {
@@ -74,6 +74,58 @@ function pageService($q, $log, $http, $window, authService) {
     })
     .catch( err => {
       $log.error(err.message);
+      return $q.reject(err);
+    });
+  };
+
+  service.joinPage = function(pageID) {
+    $log.debug('pageService.joinPage');
+
+    return authService.getToken()
+    .then( token => {
+      let url = `${__API_URL__}/api/joinpage/${pageID}`;
+      let config = {
+        headers: {
+          Accept: 'application/json',
+          Authorization: `Bearer ${token}`
+        }
+      };
+
+      return $http.get(url, config);
+    })
+    .then( res => {
+      $log.log('Page joined', res);
+      let page = res.data;
+      return page;
+    })
+    .catch( err => {
+      $log.error('Page not joined',err.message);
+      return $q.reject(err);
+    });
+  };
+
+  service.leavePage = function(pageID) {
+    $log.debug('pageService.leavePage');
+
+    return authService.getToken()
+    .then( token => {
+      let url = `${__API_URL__}/api/leavepage/${pageID}`;
+      let config = {
+        headers: {
+          Accept: 'application/json',
+          Authorization: `Bearer ${token}`
+        }
+      };
+
+      return $http.get(url, config);
+    })
+    .then( res => {
+      $log.log('Page left', res);
+      let page = res.data;
+      return page;
+    })
+    .catch( err => {
+      $log.error('Page not left',err.message);
       return $q.reject(err);
     });
   };
