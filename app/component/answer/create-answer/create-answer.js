5859dcd444ab888f9f63c411ab6e59d9a0cd9ad2
Khalid Mohamud <kmohamud1@outlook.com>
2017-06-06 00:16:15 -0700

    added func to create answer js

--- app/component/answer/create-answer/create-answer.js
+++ app/component/answer/create-answer/create-answer.js
@@ -8,6 +8,8 @@ module.exports = {
   controllerAs: 'createAnswerCtrl',
   bindings: {
     forum: '<',
+    answer: '<',
+    poll: '<'
   }
 };
 
@@ -19,9 +21,22 @@ function CreateAnswerController($log, $window, answerService) {
   this.createAnswer = function(){
     $log.debug('createAnswerCtrl.createAnswer()');
 
-    answerService.createAnswer(this.forum._id, this.answerData)
+    if(this.forum) {
+      answerService.createAnswer(this.forum._id, this.answerData)
     .then( forum => console.log('Successfully created answer', forum))
     .catch( err => console.log('FAiled to createAnswer', err));
+    }
+    if(this.answer) {
+      answerService.replyAnswer(this.answer._id, this.answerData)
+    .then( forum => console.log('Successfully replied answer', forum))
+    .catch( err => console.log('FAiled to reply to Answer', err));
+    }
+  
+    if(this.poll) {
+      answerService.createPollAnswer(this.poll._id, this.answerData)
+    .then( forum => console.log('Successfully created poll answer', forum))
+    .catch( err => console.log('FAiled to create poll Answer', err));
+    }
   };
 
 }
