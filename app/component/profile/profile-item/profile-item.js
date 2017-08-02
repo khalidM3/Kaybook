8913a51230c9a8ae2eb7b54a879ac99e0ef42d59
Khalid Mohamud <kmohamud1@outlook.com>
2017-08-01 17:36:05 -0700

    profile adn article items change

--- app/component/profile/profile-item/profile-item.js
+++ app/component/profile/profile-item/profile-item.js
@@ -14,13 +14,6 @@ module.exports = {
 function ProfileItemController($log, $rootScope, $stateParams, $window, $uibModal, profileService, pageService, postService, roomService) {
   $log.debug('ProfileViewController');
 
-  // this.userID = $stateParams.userID;
-  // this.showEditView = false;
-
-
-  // let userID = $window.localStorage.getItem('userID');
-  
-  // this.showEditOption = userID === this.userID;
 
   this.fetchMyProfile = function(){
     $log.debug('profileItemCtrl.fetchMyProfile()');
@@ -30,12 +23,20 @@ function ProfileItemController($log, $rootScope, $stateParams, $window, $uibModa
     .catch(err => $log.error('FAILED fetchMyProfile()', err));
   };
 
+
+
   this.fetchFRP = function(){
     $log.debug('profileViewCtrl.fetchFRP()');
 
+    this.showRoomBtn = false;
+    this.showPostBtn = false;
+    // this.showProfileTile = true;
     this.showCreatePage = false;
 
+    this.fiendsPosts = [];
     this.profileArr = [];
+    this.pagesArr = [];
+    this.roomsArr = [];
 
     profileService.fetchFriendReq(this.profile._id)
    .then( profiles => this.profileArr = profiles.friendReq);
@@ -45,11 +46,15 @@ function ProfileItemController($log, $rootScope, $stateParams, $window, $uibModa
   this.fetchMyFriends = function(){
     $log.debug('profileItemCtrl.fetchMyFriends()');
 
+    this.showRoomBtn = false;
+    this.showPostBtn = false;
+    // this.showProfileTile = true;
     this.showCreatePage = false;
-    this.showProfileTile = true;
+
     this.fiendsPosts = [];
     this.profileArr = [];
     this.pagesArr = [];
+     this.roomsArr = [];
 
     profileService.fetchFriends(this.profile._id)
     .then( profiles => this.profileArr = profiles.friends)
@@ -59,12 +64,15 @@ function ProfileItemController($log, $rootScope, $stateParams, $window, $uibModa
   this.fetchPage = function(){
     $log.debug('profileItemCtrl.fetchPage()');
 
+    this.showRoomBtn = false;
+    this.showPostBtn = false;
     this.showProfileTile = false;
     this.showCreatePage = true;
 
     this.pagesArr = [];
     this.profileArr = [];
     this.friendsPosts = [];
+    this.roomsArr = [];
 
     pageService.fetchPagesByPID(this.profile._id)
     .then( pages => this.pagesArr = pages)
@@ -75,6 +83,12 @@ function ProfileItemController($log, $rootScope, $stateParams, $window, $uibModa
   this.fetchFriendsPosts = function(){
     $log.debug('profileItemCtrl.fetchFriendsPosts()');
 
+    this.showRoomBtn = false;
+    this.showPostBtn = false;
+    this.showProfileTile = false;
+    this.showCreatePage = false;
+
+
     this.pagesArr = [];
     this.profileArr = [];
     this.fiendsPosts = [];
@@ -83,6 +97,49 @@ function ProfileItemController($log, $rootScope, $stateParams, $window, $uibModa
     postService.fetchFriendsPosts()
     .then( posts => this.friendsPosts =  posts)
     .catch(err => console.log('Failed to fetch posts', err));
+  };
+  
+  this.fetchTimeline = () => {
+    $log.debug('profileItemCtrl.fetchTimeline()');
+
+    this.showRoomBtn = false;
+    this.showPostBtn = true;
+    this.showProfileTile = false;
+    this.showCreatePage = false;
+
+
+    this.pagesArr = [];
+    this.profileArr = [];
+    this.fiendsPosts = [];
+    this.roomsArr = [];
+
+    postService.fetchTimeline()
+    .then( posts => this.friendsPosts =  posts)
+    .catch(err => console.log('Failed to fetch posts', err));
+  };
+
+  this.fetchJoinedPages = () => {
+    $log.debug('profileItemCtrl.fetchJoinedPages()');
+
+    this.showRoomBtn = false;
+    this.showPostBtn = false;
+    this.showProfileTile = false;
+    this.showCreatePage = true;
+
+
+    this.pagesArr = [];
+    this.profileArr = [];
+    this.fiendsPosts = [];
+    this.roomsArr = [];
+    
+    profileService.fetchJoinedPages()
+    .then( profile =>  {
+      this.myProfile = profile;
+      this.pagesArr = profile.memberOf;
+      console.log('this.pagesArr', this.pagesArr, profile.memberOf);
+      return;
+    })
+    .catch(err => $log.error('FAILED fetchJoiedPages()', err));
   };
 
   this.bark = function(){
@@ -108,6 +165,12 @@ function ProfileItemController($log, $rootScope, $stateParams, $window, $uibModa
   this.fetchMyRooms = function(){
     $log.debug('profileItemCtrl.fetchMyRooms()');
 
+    this.showRoomBtn = true;
+    this.showPostBtn = false;
+    this.showProfileTile = false;
+    this.showCreatePage = false;
+
+
     this.pagesArr = [];
     this.profileArr = [];
     this.friendsPosts = [];
@@ -116,88 +179,10 @@ function ProfileItemController($log, $rootScope, $stateParams, $window, $uibModa
     .then( room => this.roomsArr = room);
   };
 
-  // this.check = function(){
-  //   $log.debug('profileViewController.check()');
 
-  //   let userID = $window.localStorage.getItem('userID');
-  //   profileService.fetchProfile(userID)
-  //   .then( profile => {
-  //     let arr = profile.memberOf;
-  //     this.showLeaveBtn = arr.some( PID => PID.toString() === this.userID.toString());
-  //     // console.log('profile.memberOf  }-------->',arr);
-  //     // console.log('}------->', this.userID);
-  //   });
-  // };
-  // this.check();
-
-
-  
-  // this.updateProfileView = function() {
-  //   $log.debug('ProfileViewController.updateProfileView()');
-  //   console.log('this.postArray:::',this.postsArray);
-
-  //   this.postsArray = [];
-    
-  //   profileService.fetchProfile(this.userID)
-  //   .then(profile => {
-  //     this.profile = profile;
-  //     this.showEditView = false;
-  //     if(profile.posts.length !== 0){
-  //       profile.posts.forEach( profileID => {
-  //         postService.fetchPost(profileID)
-  //         .then( postObj => this.postsArray.push(postObj));
-  //       });
-  //     }
-  //   })
-  //   .catch( err => $log.error(err.message));
-  // };
-
-  // this.join = function(){
-  //   $log.debug('ProfileViewController.join()');
-
-  //   profileService.joinProfile(this.userID)
-  //   .then( res => console.log('SUCCESS join()', res))
-  //   .catch( err => console.error('FAILED join()', err));
-  //   return this.updateProfileView();
-  // };
-
-  // this.leave = function(){
-  //   $log.debug('ProfileViewController.leave()');
-
-  //   profileService.leaveProfile(this.userID)
-  //   .then( profile => console.log(profile))
-  //   .catch( (err) => console.error('FAILED leave()', err));
-  // };
-
-  // this.open = function (size, selectedPost) {
-  //   // var parentElem = parentSelector ? 
-  //   //   angular.element($document[0].querySelector('.modal-demo ' + parentSelector)) : undefined;
-  //   var modalInstance = $uibModal.open({
-  //     animation: this.animationsEnabled,
-  //     // ariaLabelledBy: 'modal-title',
-  //     // ariaDescribedBy: 'modal-body',
-  //     templateUrl: 'create-post.html',
-  //     // controller: 'CreatePostController',
-  //     // controllerAs: 'this',
-  //     size: size,
-  //     // appendTo: parentElem,
-  //     resolve: {
-  //       items: function () {
-  //         return selectedPost;
-  //       }
-  //     }
-  //   });
-
-  //   // modalInstance.result.then(function (selectedItem) {
-  //   //   this.selected = selectedItem;
-  //   // }, function () {
-  //   //   $log.info('Modal dismissed at: ' + new Date());
-  //   // });
-  // };
-
-  this.openCreatePostModal= function ( profile) {
-
-    var modalInstance = $uibModal.open({
+  this.openCreatePostModal= function () {
+    let profile = this.profile;
+    $uibModal.open({
       animation: this.animationsEnabled,
       component: 'createPost',
       resolve: {
