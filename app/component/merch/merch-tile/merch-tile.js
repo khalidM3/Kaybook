d6a912e8060604ae6e26eb5d3cb44f2839ba829d
Khalid Mohamud <kmohamud1@outlook.com>
2018-02-11 18:45:22 -0800

    tile change

--- app/component/merch/merch-tile/merch-tile.js
+++ app/component/merch/merch-tile/merch-tile.js
@@ -1,6 +1,42 @@
 'use strict';
 
-require('./_merch-tile.scss');
+import './_merch-tile.scss';
+
+class MerchTileController {
+  constructor($log, $location, $uibModal, $window, merchService, picService, profileService) {
+    this.$log = $log;
+    this.$location = $location;
+    this.$uibModal = $uibModal;
+    this.$window = $window;
+    this.merchService = merchService;
+    this.picService = picService;
+    this.profileService = profileService;
+  }
+
+  $onInit() {
+    this.profile = JSON.parse(this.$window.localStorage.profile);
+  }
+
+
+  goToMerch () {
+    let merch = this.merch;
+    this.$location.search('mid', merch._id);
+    this.$uibModal.open({
+      animation: this.animationsEnabled,
+      component: 'merchItem',
+      resolve: {
+        merch: function () {
+          return merch; 
+        }
+      }
+    })
+    .closed.then( () => {
+      this.$location.search('mid', null);
+    })
+  }
+
+}
+
 
 module.exports = {
   template: require('./merch-tile.html'),
@@ -9,42 +45,40 @@ module.exports = {
   bindings: {
     merch: '<'
   }
-};
-
-
-function MerchTileController($log, $location, $uibModal, $window, merchService, picService, profileService){
-  $log.debug('MerchTileController');
-
-  let profileID = $window.localStorage.getItem('profileID');
-
-  this.$onInit = function(){
-    $log.debug('merchTileCtrl.$onInit()');
-
-    this.isVid = (/\.mp4$/).test(this.merch.merchPicURI);
-    this.isMyMerch = this.merch.posterID.toString() === profileID.toString();
-
-    profileService.fetchProfile2(this.merch.posterID)
-    .then(profile => {
-      console.log('PPPPPPPPP', profile);
-      this.mercher = profile;
-    });
-  };
-
-  this.openMerchModal = function () {
-    let merch = this.merch;
-    $location.search('mid', merch._id);
-    $uibModal.open({
-      animation: this.animationsEnabled,
-      component: 'merchItem',
-      resolve: {
-        merch: function () {
-          return merch; 
-        }
-      }
-    });
-  };
-
 }
+
+// function MerchTileController($log, $location, $uibModal, $window, merchService, picService, profileService){
+//   $log.debug('MerchTileController');
+
+
+//   this.$onInit = function(){
+//     $log.debug('merchTileCtrl.$onInit()');
+
+//     this.isVid = (/\.mp4$/).test(this.merch.merchPicURI);
+//     this.isMyMerch = this.merch.posterID.toString() === profileID.toString();
+
+//     profileService.fetchProfile2(this.merch.posterID)
+//     .then(profile => {
+//       console.log('PPPPPPPPP', this.merch);
+//       this.mercher = profile;
+//     });
+//   };
+
+//   this.openMerchModal = function () {
+//     let merch = this.merch;
+//     $location.search('mid', merch._id);
+//     $uibModal.open({
+//       animation: this.animationsEnabled,
+//       component: 'merchItem',
+//       resolve: {
+//         merch: function () {
+//           return merch; 
+//         }
+//       }
+//     });
+//   };
+
+// }
 
 
 
