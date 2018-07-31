9c943fa75293ba00512e2a024766d579c0f0aef1
Khalid Mohamud <kmohamud1@outlook.com>
2018-07-30 20:23:00 -0700

    cleanup all uncommented and unneded console logs

--- app/component/account/account-pages/account-pages.js
+++ app/component/account/account-pages/account-pages.js
@@ -2,25 +2,19 @@
 
 require('./_account-pages.scss');
 
-// module.exports = ['$log', '$rootScope', '$stateParams', 'profileService', 'postService', 'commentService', AccountprofileController];
-
 module.exports = {
   template: require('./account-pages.html'),
-  controller: ['$log', '$rootScope', '$stateParams', '$window', 'profileService', 'postService', 'pageService', AccountPagesController],
+  controller: ['$log', '$window', 'pageService', AccountPagesController],
   controllerAs: 'accountPagesCtrl',
 };
 
-function AccountPagesController($log, $rootScope, $stateParams, $window, profileService, postService, pageService) {
+function AccountPagesController($log, $window, pageService) {
   $log.debug('AccountPagesController');
 
 
   this.$onInit = () => {
     this.profile = JSON.parse($window.localStorage.profile);
     this.fetchPages();
-  };
-
-  this.bark = () => {
-    console.log(profileService.profile);
   };
 
   this.fetchPages = () => {
@@ -34,7 +28,6 @@ function AccountPagesController($log, $rootScope, $stateParams, $window, profile
 
   this.changeCurr = (page) => {
     this.currpage = page;
-    console.log('curr page :',this.currpage);
   };
 
 
